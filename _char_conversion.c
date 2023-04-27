2c02d2571bfe26635b8de561b2804c0b8d024bb8
rah102993 <raheemahlartey2012@yahoo.com>
2023-04-27 16:06:26 -0700

    Task 0, third commit.

--- _char_conversion.c
+++ _char_conversion.c
@@ -8,10 +8,10 @@
  */
 int is_digit(char c)
 {
-        if (c >= '0' && c <= '9')
-                return (1);
+	if (c >= '0' && c <= '9')
+		return (1);
 
-        return (0);
+	return (0);
 }
 
 /**
@@ -23,12 +23,12 @@ int is_digit(char c)
  */
 long int convert_size_number(long int num, int size)
 {
-        if (size == S_LONG)
-                return (num);
-        else if (size == S_SHORT)
-                return ((short)num);
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
 
-        return ((int)num);
+	return ((int)num);
 }
 
 /**
@@ -40,10 +40,10 @@ long int convert_size_number(long int num, int size)
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
-        if (size == S_LONG)
-                return (num);
-        else if (size == S_SHORT)
-                return ((unsigned short)num);
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
 
-        return ((unsigned int)num);
+	return ((unsigned int)num);
 }
